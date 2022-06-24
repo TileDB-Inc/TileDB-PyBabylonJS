2f415aa8ef53b02fe1bdbc568a49ec806a02648e
SarantopoulosKon <33217757+SarantopoulosKon@users.noreply.github.com>
2022-06-24 13:30:08 +0300

    Enable publishing beta versions (#32)
    * Enable publishing beta versions
    
    * Get module version from package.json's version
    
    * Use npm version as module_version instead of the pypi, since they follow different format for prereleases

--- pybabylonjs/_frontend.py
+++ pybabylonjs/_frontend.py
@@ -3,10 +3,11 @@
 
 # Copyright (c) TileDB, Inc..
 # Distributed under the terms of the Modified BSD License.
+from ._version import __npm_version__
 
 """
 Information about the frontend package of the widgets.
 """
 
 module_name = "@tiledb-inc/pybabylonjs"
-module_version = ">=1.0.4"
+module_version = __npm_version__
