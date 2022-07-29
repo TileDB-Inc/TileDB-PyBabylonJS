0465bb37c2ea4d8cce4497f6df08ff35d10c0c88
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2022-07-29 14:24:28 +0100

    shift points to [0,0,0] origin by default

--- src/data.ts
+++ src/data.ts
@@ -43,16 +43,34 @@ export async function getPointCloud(values: any) {
     data = dataIn;
   }
 
+  let { xmin, xmax, ymin, ymax, zmin, zmax, rgbMax } = getPointCloudLimits(
+    values,
+    data
+  );
+     
+  // shift points to new origin of [0,0,0] with [xmin,ymin,zmin]
+  data.X = data.X.map((n: any) => n - xmin);
+  data.Y = data.Y.map((n: any) => n - ymin);
+  data.Z = data.Z.map((n: any) => n - zmin);
+  xmax = xmax - xmin;
+  xmin = 0;
+  ymax = ymax - ymin;
+  ymin = 0;
+  zmax = zmax - zmin;
+  zmin = 0;
+  
+  // shift points with user defined values (optional)
   if (values.point_shift[0]) {
     data.X = data.X.map((n: any) => n + values.point_shift[0]);
     data.Y = data.Y.map((n: any) => n + values.point_shift[1]);
     data.Z = data.Z.map((n: any) => n + values.point_shift[2]);
-  }
-
-  const { xmin, xmax, ymin, ymax, zmin, zmax, rgbMax } = getPointCloudLimits(
-    values,
-    data
-  );
+    xmin = xmin + values.point_shift[0];
+    xmax = xmax + values.point_shift[0];
+    ymin = ymin + values.point_shift[1];
+    ymax = ymax + values.point_shift[1];
+    zmin = zmin + values.point_shift[2];
+    zmax = zmax + values.point_shift[2];
+    }
 
   return { data, xmin, xmax, ymin, ymax, zmin, zmax, rgbMax };
 }
