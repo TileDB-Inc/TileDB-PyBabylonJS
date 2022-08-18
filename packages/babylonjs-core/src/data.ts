1e3974088fa652627d14b37c8e1e2e933bb4b679
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2022-08-18 16:28:21 +0300

    Move cloud point visualization to core package

--- packages/babylonjs-core/src/data.ts
+++ packages/babylonjs-core/src/data.ts
@@ -24,30 +24,28 @@ export function setPointCloudSwitches(mode: string) {
 }
 
 export function setSceneColors(colorScheme: string) {
-
   let backgroundColor: Color4;
   let accentColor: string;
   let secondColor: string;
   let textColor: string;
 
-  backgroundColor = new Color4(0, 24/255, 92/255, 1);
+  backgroundColor = new Color4(0, 24 / 255, 92 / 255, 1);
   accentColor = '#CC0055';
   secondColor = '#C7C7C7';
   textColor = '#F5F7FA';
   if (colorScheme === 'dark') {
-    backgroundColor = new Color4(28/255, 28/255, 28/255, 1);
+    backgroundColor = new Color4(28 / 255, 28 / 255, 28 / 255, 1);
     accentColor = '#C7C7C7';
     secondColor = '#F5F7FA';
     textColor = '#F5F7FA';
   }
   if (colorScheme === 'light') {
-    backgroundColor = new Color4(245/255, 247/255, 250/255, 1);
+    backgroundColor = new Color4(245 / 255, 247 / 255, 250 / 255, 1);
     accentColor = '#352F4D';
     secondColor = '#C7C7C7';
     textColor = '#352F4D';
   }
   return { backgroundColor, accentColor, secondColor, textColor };
-
 }
 
 export async function getPointCloud(values: any) {
@@ -75,7 +73,7 @@ export async function getPointCloud(values: any) {
     values,
     data
   );
-     
+
   // shift points to new origin of [0,0,0] with [xmin,ymin,zmin]
   data.X = data.X.map((n: any) => n - xmin);
   data.Y = data.Y.map((n: any) => n - ymin);
@@ -86,7 +84,7 @@ export async function getPointCloud(values: any) {
   ymin = 0;
   zmax = zmax - zmin;
   zmin = 0;
-  
+
   // shift points with user defined values (optional)
   if (values.point_shift[0]) {
     data.X = data.X.map((n: any) => n + values.point_shift[0]);
@@ -98,7 +96,7 @@ export async function getPointCloud(values: any) {
     ymax = ymax + values.point_shift[1];
     zmin = zmin + values.point_shift[2];
     zmax = zmax + values.point_shift[2];
-    }
+  }
 
   return { data, xmin, xmax, ymin, ymax, zmin, zmax, rgbMax };
 }
